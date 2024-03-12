95c4e1fc43a842787dc18ac4fd4ccdfcea647d8c
Amir <Amirtzi2072@gmail.com>
2024-03-12 18:17:03 +0200

    updates

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -15,6 +15,7 @@ function Projects() {
         </h1>
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
+          And about the others you can read my CV or simply ask me
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
