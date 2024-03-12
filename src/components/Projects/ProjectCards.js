28aff084bf1e4ab37ea845927b291e38a9f9cfd8
Amir <Amirtzi2072@gmail.com>
2024-03-12 17:45:43 +0200

    create the secode project

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -12,13 +12,10 @@ function ProjectCards(props) {
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
+          {"\n"}
+        {"\n"}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
+       
 
         {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
 
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,13 +2,8 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
 import unicore from "../../Assets/Projects/UnicoreDash.png"
+import monitor from "../../Assets/Projects/monitoring-png.png"
 
 function Projects() {
   return (
@@ -28,6 +23,14 @@ function Projects() {
               isBlog={false}
               title="Unicore"
               description="Unicore is an innovative project aimed at transforming IT and DevOps operations within organizations by leveraging advanced language models to streamline processes and enhance efficiency. At its core, Unicore serves as a powerful tool that studies an organization's environment and existing integrations comprehensively, enabling IT and DevOps professionals to execute actions seamlessly across various systems and automations."
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={monitor}
+              isBlog={false}
+              title="Git-Monitor"
+              description="The GitHub Monitor is a dynamic tool designed to keep you informed about any updates or modifications in your GitHub repositories as they happen. Whether it's a new pull request, code review request, or any other action, our monitor ensures you're always in the loop."
             />
           </Col>
         </Row>
